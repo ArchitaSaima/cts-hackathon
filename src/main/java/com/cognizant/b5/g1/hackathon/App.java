cdfe4fdadf431985776fc638accb5b30640efe1c
Im-CS <53789496+Im-CS@users.noreply.github.com>
2021-04-23 15:53:52 +0530

    Add files via upload

---
+++ src/main/java/com/cognizant/b5/g1/hackathon/App.java
@@ -0,0 +1,92 @@
+package com.cognizant.b5.g1.hackathon;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.*;
+
+public class App 
+{
+	
+	private static WebDriver driver;
+	private static Properties objPro = new Properties();
+	private static XSSFWorkbook wb;
+	
+	@BeforeSuite
+	public void setupProperties() {
+
+		try {
+			FileInputStream objFis = new FileInputStream(".\\src\\prop.properties");
+			objPro.load(objFis);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	//Data Provider
+	@DataProvider
+	public void readExcel() {
+		//For excel
+	}
+	
+	//To setup the driver
+	@BeforeTest
+	public void setupDriver(){
+		
+		objPro.getProperty("browser");
+		String browserName = objPro.getProperty("browser");
+
+		if (browserName.equalsIgnoreCase("Chrome")) {
+			System.setProperty("webdriver.chrome.driver", ".\\drivers\\chromedriver.exe");
+			driver = new ChromeDriver();
+
+			driver.manage().window().maximize();
+		} else if (browserName.equalsIgnoreCase("Firefox")) {
+			System.setProperty("webdriver.gecko.driver", ".\\drivers\\geckodriver.exe");
+			driver = new FirefoxDriver();
+
+			driver.manage().window().maximize();
+		} else {
+			System.out.println("browser is not correct");
+			System.exit(0);
+		}
+		
+		driver.manage().timeouts().implicitlyWait(250, TimeUnit.SECONDS);
+		
+	}
+	
+	@Test
+	public void CarEMI() {
+		//For point 1
+	}
+	
+	@Test
+	public void HomeEMI() {
+		//For point 2
+	}
+	
+	@Test
+	public void UIValidation() {
+		//For point 3
+	}
+	
+	
+	//To close Browser
+	@AfterSuite
+	public void closeBrowser() {
+		driver.close();
+	}
+	
+    public static void main( String[] args )
+    {
+        App a = new App();
+        a.setupDriver();
+        a.closeBrowser();
+    }
+}
